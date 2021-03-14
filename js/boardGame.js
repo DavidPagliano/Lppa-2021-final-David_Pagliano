5caa6d7e3afa1e21ed30ac20f61dc1e4b5b947e2
DavidPagliano <paglianodavid@gmail.com>
2021-03-14 14:28:57 -0300

    Create saved.js and create of funtion check and winner

--- js/boardGame.js
+++ js/boardGame.js
@@ -1,66 +1,38 @@
-var boardHTML = null;
-
-var columns = null;
-
-/**
- * Matriz para que jueguen dos jugadores
- */
-var board2 = [
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, players, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null]
-]
-
-var board3 = [
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, players, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null]
-]
-
-//turno de los jugadores
-var turns = function () {
-    turn = (turn === 'p1') ? 'p2' : 'p1';
+class boardGame {
+    constructor(board, boardHTML, columnHTML) {
+        this.board = board,
+            this.boardHTML = boardHTML,
+            this.columnHTML = columnHTML;
+    }
 }
 
-var render = function () {
+reset = function () {
+    for (var i = 0; i < this.board.length; i++) {
+        for (var j = 0; j < this.board[i].length; j++) {
+            this.board[i][j] = null;
+        }
+    }
+}
+
+bindColumnHandlers = function () {
+    for (var i = 0; i < this.columnsHTML.length; i++) {
+        this.columnsHTML[i].onclick = this.columnEventHandler.bind(this);
+    }
+}
+
+
+render = function () {
     var html = '';
-    for (var i = 0; i < board2.length; i++) {
-        html += '<div id="c' + i + 'class=column>';
-        for (var j = board2[i].length - 1; j >= 0; j--) {
-            html += '<div id ="s' + i + j + '"class="spot';
-            if (board2[i][j]) html += ' ' + board2[i][j];
+    for (var i = 0; i < this.board.length; i++) {
+        (this.board.length === 10) ? html += '<div id="columns' + i + '" class="column three">'
+            : html += '<div id="columns' + i + '" class="column">';
+        for (var j = this.board[i].length - 1; j >= 0; j--) {
+            html += '<div id="slot' + i + j + '" class="slot';
+            if (this.board[i][j]) { html += ' ' + this.board[i][j]; }
             html += '"></div>';
         }
         html += '</div>';
     }
-    boardHTML.innerHTML = html;
-    bindColumnHandlers();
-}
-
-
-
-var columnEventHandler = function (evt) {
-    var columnId = evt.target.id.substr(1, 1);
-    for (var i = 0; i < board2[columnId].length; i++) {
-        if (!board2[columnId][i]) {
-            board2[columnId][i] = turn;
-            turns();
-            render();
-            break;
-        }
-    }
-}
-
-var init = function () {
-    boardHTML = document.getElementById('board');
-    turn = Math.random() > 0.5 ? 'p1' : 'p2';
+    this.boardHTML.innerHTML = html;
+    this.bindColumnHandlers();
 }
--- js/game.js
+++ js/game.js
@@ -1,94 +1,173 @@
-'use strict';
-// var player1 = { name: 'You', color: 'yellow' };
-// var player2 = { name: 'AI (Easy)', color: 'red' };
-// var status = 'ready'; // 'ready', 'p1Turn', 'p2Turn', 'p1Win', 'p2Win'
-var boardHTML = null;
-var columnsHTML = null;
-var red = null;
-boardHTML = document.getElementsByClassName('board');
-var turn = 'yellow';
-
-
-
-var toggleTurn = function () {
-    turn = (turn === 'yellow') ? 'red' : 'yellow';
+var Game = new Object();
+window.onload = function () {
+    loadGame();
 }
 
-var checkGameStatus = function (col, row) {
-    // // check horizontal
-    // if (board[col, row] === board[col, row + 1]) 
-    // if (board[col, row] === board[col, row + 2])
-    // if (board[col, row] === board[col, row + 3])
-    // if (board[col, row] === board[col, row - 1])
-    // if (board[col, row] === board[col, row - 2])
-    // if (board[col, row] === board[col, row - 3])
-
-    // // check vertical
-    // if (board[col, row] === board[col + 1, row])
-    // if (board[col, row] === board[col + 2, row])
-    // if (board[col, row] === board[col + 3, row])
-    // if (board[col, row] === board[col - 1, row])
-    // if (board[col, row] === board[col - 2, row])
-    // if (board[col, row] === board[col - 3, row])
-
-    // // check inc diagonal
-    // if (board[col, row] === board[col + 1, row + 1])
-    // if (board[col, row] === board[col + 2, row + 1])
-    // if (board[col, row] === board[col + 3, row + 1])
-    // if (board[col, row] === board[col - 1, row - 1])
-    // if (board[col, row] === board[col - 2, row - 1])
-    // if (board[col, row] === board[col - 3, row - 1])
-
-    // // check dec diagonal
-    // if (board[col, row] === board[col + 1, row - 1])
-    // if (board[col, row] === board[col + 2, row - 1])
-    // if (board[col, row] === board[col + 3, row - 1])
-    // if (board[col, row] === board[col - 1, row + 1])
-    // if (board[col, row] === board[col - 2, row + 1])
-    // if (board[col, row] === board[col - 3, row + 1])
+function loadGame() {
+    var storage = window.localStorage;
+    loadGameData(storage);
+    renderGame();
+    startGame();
 }
 
-var columnEventHandler = function (evt) {
-    var columnId = evt.target.id.substr(1, 1);
-    for (var i = 0; i < board[columnId].length; i++) {
-        if (!board[columnId][i]) {
-            board[columnId][i] = turn;
-            checkGameStatus(columnId, i);
-            toggleTurn();
-            render();
-            break
+function loadGameData(storage) {
+    loadGameConfig(storage);
+    loadPlayerConfig(storage);
+    loadBoardConfig(storage);
+}
+
+
+function loadGameConfig(storage) {
+    Game.playerCount = storage.getItem("playerCount");
+    Game.time = storage.getItem("time");
+    Game.turn = storage.getItem("turn");
+    Game.playerCount = storage.getItem("playerCount");
+    Game.maxColumns = storage.getItem("maxColumns");
+    Game.maxRows = storage.getItem("maxRows");
+}
+
+function loadPlayerConfig(storage) {
+    Game.players = new Array();
+
+    for (let i = 1; i <= Game.playerCount; i++) {
+        var player = new Object();
+        player.ID = i;
+        player.Name = storage.getItem("playerName" + i);
+        player.Color = storage.getItem("playerColor" + i);
+        Game.players.push(player);
+    }
+}
+
+function loadBoardConfig(storage) {
+    var board = new Array();
+    var boardString = storage.getItem("board");
+    var columnsString = boardString.split('|');
+
+    for (let i = 0; i < columnsString.length; i++) {
+        var rowsString = columnsString[i].split('-');
+        var column = new Array();
+        for (let j = 0; j < rowsString.length; j++) {
+            var row = new Object();
+            var player = Game.players.find(x => x.ID == parseInt(rowsString[j]));
+            row.Owner = player;
+            column.push(row);
         }
+        board.push(column);
     }
+
+    Game.board = board;
 }
 
-var bindColumnHandlers = function () {
-    columnsHTML = document.getElementsByClassName('column');
-    for (var i = 0; i < columnsHTML.length; i++) {
-        columnsHTML[i].onclick = columnEventHandler;
-    }
-}
 
-var render = function () {
+function renderGame() {
     var html = '';
-    boardHTML = document.getElementsByClassName('board');
+    board = document.getElementsByClassName('board');
+    boardHTML = document.getElementsByClassName('game');
     for (var i = 0; i < board.length; i++) {
-        html += '<div id="c' + i + '" class="column">';
-        for (var j = board[i].length - 1; j >= 0; j--) {
-            html += '<div id="s' + i + j + '" class="spot';
-            if (board[i][j]) html += ' ' + board[i][j];
+        this.board.length === 10 ? html += '<div id="column' + i + '" class="column3">'
+            : html += '<div id="column' + i + '"class="column">';
+        for (var j = this.board[i] - 1; j >= 0; j--) {
+            html += '<div id="slot' + i + j + '" class="slot';
+            if (this.board[i][j]) { html += ' ' + this.board[i][j]; }
             html += '"></div>';
         }
-        html += '</div>';
+        html += '></div>';
+    }
+    this.boardHTML.innerHTML = html;
+    this.bindColumnHandlers();
+
+
+    /*var table = document.createElement("table");
+
+    for (let i = 0; i < Game.board.length; i++) {
+        var column = document.createElement("tr");
+
+        for (let j = 0; j < Game.board[i].length; j++) {
+            var row = document.createElement("td");
+            var circle = document.createElement("div");
+            circle.classList.add("spot");
+            circle.style.backgroundColor = Game.board[i].row[j].Owner.Color;
+            row.append(circle);
+            column.append(row);
+        }
+        table.append(column);
     }
 
-    boardHTML.innerHTML = html;
-    bindColumnHandlers();
+    boardElement.append(table);*/
 }
 
-var init = function () {
-    boardHTML = document.getElementsByClassName('board');
-    turn = Math.random() > 0.5 ? 'yellow' : 'red';
-    render();
+function winner() {
+    var saved = false;
+    var winner = function (playersNames) {
+        status.className = ' ';
+        boardHTML.className += 'disabled';
+        if (saved) {
+            winnerPoster.innerHTML = '';
+            messagePoster.innerHTML = '¡Saved the game!';
+            resetButton.innerHTML = 'Accept';
+        } else {
+            var gameOver = null;
+            gameOver = true;
+            if (playerNames) {
+                playerNames = (playerNames == 'p1') ? player.p1 : player.p2;
+                winnerPoster.innerHTML = playerNames;
+                messagePoster.innerHTML = 'The winner is ' + players + '!!!!';
+            } else {
+                winnerPoster.innerHTML = 'No player has won!';
+                messagePoster.innerHTML = 'It\'s a TIE!';
+            }
+            resetButton.innerHTML = '¡Play again!';
+        }
+    }
 }
 
-window.onload = init();
+function check() {
+
+    //Chequeo de forma vertical
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < 4; j++) {
+            if (board[i][j]) {
+                if (board[i][j] === (board[i][j + 1]) && board[i][j] === board[i][j + 2] &&
+                    board[i][j] === (board[i][j + 3])) {
+                    winner(board[i][j]);
+                }
+            }
+        }
+    }
+
+    for (var i = 0; i < board.length - 3; i++) {
+        for (var j = 0; j < 4; j++) {
+            if (board[i][j]) {
+                //Chequeo de forma horizontal
+                if (board[i][j] === (board[i + 1][j]) && board[i][j] === board[i + 2][j] &&
+                    board[i][j] === (board[i + 3][j])) {
+                    winner(board[i][j]);
+                }
+
+                //Chequeo de forma diagonal en incremento
+                if (board[i][j] === (board[i + 1][j + 1]) && board[i][j] === board[i + 2][j + 2] &&
+                    board[i][j] === (board[i + 3][j + 3])) {
+                    winner(board[i][j]);
+                }
+            }
+        }
+    }
+
+    //Chequeo de forma diagonal en descreciente
+    for (var i = 0; i < board.length - 3; i++) {
+        for (var j = 3; j < board[i].length; j++) {
+            if (board[i][j]) {
+                if (board[i][j] === (board[i + 1][j - 1]) && board[i][j] === board[i + 2][j - 2] &&
+                    board[i][j] === (board[i + 3][j - 3])) {
+                    winner(board[i][j]);
+                }
+            }
+        }
+    }
+
+}
+
+
+function startGame() {
+
+}
