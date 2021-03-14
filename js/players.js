087b8dcc5dd4fd383685b01915cee2cc21f13350
DavidPagliano <paglianodavid@gmail.com>
2021-03-14 00:52:23 -0300

    update player.js file and game.html file

--- js/players.js
+++ js/players.js
@@ -1,25 +1,16 @@
-'use strict';
+"use strict";
 
-var players = new Array();
+var playerCount = 0;
+var baseMaxColumns = 7;
+var baseMaxRows = 6;
 
+var errors = false;
 
-class player {
-    constructor(name, color) {
-        this.name = name;
-        this.color = color;
-    }
-
-}
-
-var createNewPlayers = function (name, color) {
-    var newPlayer = new player(name, color);
-    players.push(newPlayer);
-}
 function addPlayer() {
     var p3 = document.getElementById('board-p3');
     p3.style.display = 'flex';
     document.getElementById('less').style.display = 'flex';
-    document.getElementById('less').style.visibility = 'visible';
+    document.getElementById('less').style.visibility = 'visible'; // Cuando les da click el boton - aparece y el boton + desaparece
     document.getElementById('add').style.visibility = 'hidden';
 }
 
@@ -27,91 +18,109 @@ function lessPlayer() {
     var p3 = document.getElementById('board-p3');
     p3.style.display = 'none';
     document.getElementById('add').style.display = 'flex';
-    document.getElementById('less').style.visibility = 'hidden';
+    document.getElementById('less').style.visibility = 'hidden'; // Se hace la inversa y se mantiene la logica
     document.getElementById('add').style.visibility = 'visible';
-}
-
-function playercount() {
-    for (var i = null; i < players; i++) {
-        playername + i;
-    }
 }
 
 
 // Funcion para crear y guardar jugadores para luego usarlo en el juego
 function createNewGame() {
-    var miStorage = window.localStorage;
-    var playerCount = 0;
+    var storage = window.localStorage;
+    //CLEAR LOCAL STORAGE
+    storage.clear();
 
-    miStorage.clear();
-    var p1 = document.getElementById('p1').value;
-    if (!validateName(p1, 1)) {
+    //PLAYER DATA INITIALIZE
+    createPlayer(getIDValue("p1"), 0xff0000ff);
+    createPlayer(getIDValue("p2"), 0xff0000ff);
+    if (isVisible("board-p3")) createPlayer(getIDValue("p3"), 0xff0000ff);
+
+    if (hasErrors()) {
+        clearErrors();
         return;
     }
 
-    var p2 = document.getElementById('p2').value;
-    if (!validateName(p2, 2)) {
-        return;
-    }
+    setDefaultGameConfig();
 
-    var p3 = null;
-    var estilo = document.getElementById('board-p3').style.display;
-    if (estilo !== "none" && estilo !== '') {
-        p3 = document.getElementById('p3').value;
-        if (!validateName(p3, 3)) {
-            return;
-        }
-        playerCount = 3;
-    } else {
-        playerCount = 2;
-    }
+    setBoardConfig();
 
-    miStorage.setItem('playerCount', playerCount);
-    miStorage.setItem('playerName1', p1);
-    miStorage.setItem('playerColor1', 0xff0000ff);
-    miStorage.setItem('playerName2', p2);
-    miStorage.setItem('playerColor2', 0x0000ffff);
-    miStorage.setItem('Turn', 1);
-    miStorage.setItem('Time', 60);
-
-
-    if (playerCount === 3) {
-        var maxColumns = 10, maxRows = 9;
-        miStorage.setItem('playerName3', p3);
-        miStorage.setItem('playerColor3', 0x00ff00ff);
-        miStorage.setItem('maxColumns', maxColumns);
-        miStorage.setItem('maxRows', maxRows);
-        var matrixstring = '';
-        for (let i = 0; i < maxColumns; i++) {
-            for (let j = 0; j < maxRows; j++) {
-                // permite guardar las columnas y filas dependiendo la cantidad de jugadores
-                matrixstring = matrixstring + '0' + j == maxRows - 1 ? '' : '-';
-            }
-            matrixstring = matrixstring + i == maxColumns - 1 ? '' : '|';
-        }
-        miStorage.setItem('board', matrixstring);
-    } else {
-        var maxColumns = 2, maxRows = 2;
-        miStorage.setItem('maxColumns', maxColumns);
-        miStorage.setItem('maxRows', maxRows);
-        var matrixstring = "";
-        for (let i = 0; i < maxColumns; i++) {
-            for (let j = 0; j < maxRows; j++) {
-                // permite guardar las columnas y filas dependiendo la cantidad de jugadores
-                matrixstring = matrixstring + '0' + j == maxRows - 1 ? '' : '-';
-            }
-            matrixstring = matrixstring + i == maxColumns - 1 ? '' : '|';
-        }
-        miStorage.setItem('board', matrixstring);
-    }
     // redirige toda la configuracion a game.html para utilizar los datos guardados
-    window.location.href = 'game.html';
+    window.location.href = "game.html";
 
 }
 
-function validateName(p1, numero) {
-    if (p1 === '' || p1 === null || p1.lenght > 3) {
-        alert('el nombre del jugador ' + numero + ' es invalido');
+function isVisible(id) {
+    var display = document.getElementById(id).style.display;
+    if (display !== "none" && display !== "") {
+        return true;
+    }
+    return false;
+}
+
+function getIDValue(id) {
+    return document.getElementById(id).value;
+}
+
+function createPlayer(name, color) {
+    playerCount++;
+    var currentPlayerNumber = playerCount;
+    var storage = window.localStorage;
+
+    if (name === "" || name === null || name.lenght > 3) {
+        alert("The player " + currentPlayerNumber + "'s name is invalid.");
+        setErrors();
+        return;
+    }
+
+    storage.setItem("playerName" + currentPlayerNumber, name);
+    storage.setItem("playerColor" + currentPlayerNumber, color);
+}
+
+function setBoardConfig() {
+    var maxColumns = baseMaxColumns + (playerCount - 1) * 3;
+    var maxRows = baseMaxRows + (playerCount - 1) * 3;
+    var boardString = "";
+    var storage = window.localStorage;
+
+    storage.setItem("maxColumns", maxColumns);
+    storage.setItem("maxRows", maxRows);
+
+    for (let i = 0; i < maxColumns; i++) {
+        for (let j = 0; j < maxRows; j++) {
+            // permite guardar las columnas y filas dependiendo la cantidad de jugadores
+            var result = "0" + (j == maxRows - 1 ? "" : "-");
+            boardString += result;
+        }
+        boardString += i == maxColumns - 1 ? "" : "|";
+    }
+
+    storage.setItem("board", boardString);
+}
+
+function hasErrors() {
+    if (errors) {
+        return true;
+    }
+    return false;
+}
+
+function clearErrors() {
+    errors = false;
+}
+
+function setErrors() {
+    errors = true;
+}
+
+function setDefaultGameConfig() {
+    var storage = window.localStorage;
+
+    storage.setItem("turn", 1);
+    storage.setItem("time", 60);
+}
+
+function validateName(name, number) {
+    if (name === "" || name === null || name.lenght > 3) {
+        alert("el nombre del jugador " + number + " es invalido");
         return false;
     }
     return true;
